92e57a0927149fe26651fe7c658bf410ed6d253b
Mariusz11 <mariusz.muzyka@gmail.com>
2019-03-08 15:04:53 +0100

    Update registration.php

--- Test/Advice/registration.php
+++ Test/Advice/registration.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * @author Amasty Team
- * @copyright Copyright (c) 2018 Amasty (https://www.amasty.com)
- * @package Amasty_Base
- */
-
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Test_Advice',
